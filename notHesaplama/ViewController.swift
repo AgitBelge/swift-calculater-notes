cb73681d9a4bdef88db3eeb44c643890d63c6d10
Agit belge <agitbelge271@gmail.com>
2023-10-21 11:11:00 +0300

    first commit

--- notHesaplama/ViewController.swift
+++ notHesaplama/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var midtermText: UITextField!
+    
+    @IBOutlet weak var finalText: UITextField!
+    
+    @IBOutlet weak var noteLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        noteLabel.text = ""
+        
+        let getRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        view.addGestureRecognizer(getRecognizer)
     }
 
 
+    @IBAction func calculateNote(_ sender: Any) {
+        
+        let midterm = Int(midtermText.text!)!
+        let final = Int(finalText.text!)!
+        
+        let not = ((Double(midterm) / 100.0) * 40.0) + ((Double(final) / 100.0) * 60.0)
+        noteLabel.text = "notun :\(Int(not))"
+        
+    }
+    @objc func hideKeyboard(){
+        view.endEditing(true)
+    }
 }
 
